7150a61ff94a9ee253cde933dc4e2c0999fd5bef
Michael Stucki <stcuthbertus@gmail.com>
2025-06-22 21:00:37 -0600

    add constructor

--- src/Employee.java
+++ src/Employee.java
@@ -1,51 +1,40 @@
 public class Employee {
-    private int employeeID;
+    private int id;
     private String firstName;
     private String lastName;
     private int dept;
     private double payRate;
 
-    public String getFullName() {
-        return firstName + " " + lastName;
+    public Employee(String firstName, String lastName, int dept, double payRate) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setDept(dept);
+        setPayRate(payRate);
     }
+
+    public Employee() {}
+
+    public int getId() { return id; }
+
+    public String getFirstName() { return firstName; }
+
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public int getDept() { return dept; }
+
+    public void setDept(int dept) { this.dept = dept; }
+
+    public double getPayRate() { return payRate; }
+
+    public void setPayRate(double payRate) { this.payRate = payRate; }
+
+    public String getFullName() { return firstName + " " + lastName; }
 
     public String getPayInfo() {
-        return getFullName() + ", " + employeeID + ", " + dept + ", " + payRate;
-    }
-
-    public int getEmployeeID() {
-        return employeeID;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getDept() {
-        return dept;
-    }
-
-    public void setDept(int dept) {
-        this.dept = dept;
-    }
-
-    public double getPayRate() {
-        return payRate;
-    }
-
-    public void setPayRate(double payRate) {
-        this.payRate = payRate;
+        return "Employee " + id + " dept " + dept + " " + getFullName() + " paid " + payRate;
     }
 }
--- src/Payroll.java
+++ src/Payroll.java
@@ -1,11 +1,13 @@
 public class Payroll {
     public static void main(String[] args) {
-        Employee employee = new Employee();
-        employee.setFirstName("John");
-        employee.setLastName("Doe");
-        employee.setDept(1);
-        employee.setPayRate(100.0);
+        Employee e1 = new Employee();
+        e1.setFirstName("John");
+        e1.setLastName("Doe");
+        e1.setDept(2);
+        e1.setPayRate(6000.0);
+        System.out.println(e1.getPayInfo());
 
-        System.out.println(employee.getPayInfo());
+        Employee e2 = new Employee("Jane", "Smith", 15, 6500.0);
+        System.out.println(e2.getPayInfo());
     }
 }
