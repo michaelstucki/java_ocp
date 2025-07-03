98c0923f4257ff46b153d20b1f3e7952423deb2b
Michael Stucki <stcuthbertus@gmail.com>
2025-07-03 10:44:59 -0600

    reformat

--- src/employees/Employee.java
+++ src/employees/Employee.java
@@ -1,8 +1,7 @@
 package employees;
 
-public class Employee extends Person {
+public abstract class Employee extends Person {
     private static int nextId = 1;
-
     private int id = nextId++;
     private int dept;
     private double payRate;
@@ -18,7 +17,5 @@ public class Employee extends Person {
     public void setDept(int dept) { if (dept > 0) this.dept = dept; }
     public double getPayRate() { return payRate; }
     public void setPayRate(double payRate) { if (payRate >= 0) this.payRate = payRate; }
-    public String getPayInfo() {
-        return "employees.Employee " + id + " dept " + dept + " " + getFullName() + " paid " + payRate;
-    }
+    public abstract String getPayInfo();
 }
--- src/employees/Person.java
+++ src/employees/Person.java
@@ -1,6 +1,6 @@
 package employees;
 
-public class Person {
+public abstract class Person {
     private String firstName;
     private String lastName;
 
